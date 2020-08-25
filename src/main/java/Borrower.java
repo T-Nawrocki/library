0610a0f6959cce7b8e03851c2dd9aa4c9cd63ad9
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-08-25 16:36:13 +0100

    borrower initialisation test passes

---
+++ src/main/java/Borrower.java
@@ -0,0 +1,13 @@
+public class Borrower {
+
+    private String name;
+
+    public Borrower(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
