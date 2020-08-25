48a7537dab42247fdfa8f8acca46f58f4e9bb641
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-08-25 17:26:34 +0100

    hasGenreList test written

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
 
 public class LibraryTest {
@@ -64,6 +66,23 @@ public class LibraryTest {
     @Test
     public void canTellIfBookIsNotInLibrary() {
         assertFalse(library.isBookInLibrary(book));
+    }
+
+    @Test
+    public void hasGenreList() {
+        Book book2 = new Book("The Three-Body Problem", "Cixin Liu", "sci-fi");
+        Book book3 = new Book("Excession", "Iain M Banks", "sci-fi");
+
+        library.addBook(book);
+        library.addBook(book2);
+        library.addBook(book3);
+
+        HashMap<String, Integer> expected = new HashMap<String, Integer>() {{
+            put("fantasy", 1);
+            put("sci-fi", 2);
+        }};
+
+        assertTrue(library.getGenreList().equals(expected));
     }
 
 }
