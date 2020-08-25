381bb2295d1637e9501df59497ee0c85e153cf4f
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-08-25 15:40:00 +0100

    library canAddBook test complete

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -6,15 +6,23 @@ import static org.junit.Assert.assertEquals;
 public class LibraryTest {
 
     private Library library;
+    private Book book;
 
     @Before
     public void before() {
         library = new Library();
+        book = new Book("The Shepherd's Crown", "Terry Pratchett", "fantasy");
     }
 
     @Test
     public void hasBooks() {
         assertEquals(0, library.getNumberOfBooks());
+    }
+
+    @Test
+    public void canAddBook() {
+        library.addBook(book);
+        assertEquals(1, library.getNumberOfBooks());
     }
 
 }
